cd5b15fc8f94a96531599f7549ef94f07cd7e005
Jon Hedegaard Nielsen <jon230706@gmail.com>
2026-02-27 01:25:36 +0100

    added snacks

--- init.lua
+++ init.lua
@@ -83,6 +83,7 @@ require('lazy').setup({
     }
   end,
   require 'my_dap.init',
+  require 'my_snacks',
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
