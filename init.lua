bd4189f043e3301e877a480e00232ab11ccb6a97
Jon Hedegaard Nielsen <jon230706@gmail.com>
2026-03-23 00:19:07 +0100

    das

--- init.lua
+++ init.lua
@@ -102,6 +102,26 @@ require('lazy').setup({
   --
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^8', -- Recommended
+    lazy = false, -- This plugin is already lazy
+    config = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      vim.keymap.set('n', '<leader>a', function()
+        vim.cmd.RustLsp 'codeAction' -- supports rust-analyzer's grouping
+        -- or vim.lsp.buf.codeAction() if you don't want grouping.
+      end, { silent = true, buffer = bufnr })
+      vim.keymap.set(
+        'n',
+        'K', -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+        function()
+          vim.cmd.RustLsp { 'hover', 'actions' }
+        end,
+        { silent = true, buffer = bufnr }
+      )
+    end,
+  },
   require 'my_which-key',
   require 'my_lualine',
   -- NOTE: Plugins can specify dependencies.
